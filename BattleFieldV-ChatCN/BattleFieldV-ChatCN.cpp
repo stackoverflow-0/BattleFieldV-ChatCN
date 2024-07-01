487aaafc1f711ccea1c81a55227824e7cdab844c
czh <2834395095@qq.com>
2024-07-01 20:56:03 +0800

    添加项目文件。

---
+++ BattleFieldV-ChatCN/BattleFieldV-ChatCN.cpp
@@ -0,0 +1,12 @@
+﻿// BattleFieldV-ChatCN.cpp: 定义应用程序的入口点。
+//
+
+#include "BattleFieldV-ChatCN.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ BattleFieldV-ChatCN/BattleFieldV-ChatCN.h
@@ -0,0 +1,9 @@
+﻿// BattleFieldV-ChatCN.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include <Windows.h>
+
+// TODO: 在此处引用程序需要的其他标头。
